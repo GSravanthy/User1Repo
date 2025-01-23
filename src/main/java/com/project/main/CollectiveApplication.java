155321d41ab05a98b366121840c659656e9a4f1a
sravanthi <gsravanthi657@gmail.com>
2025-01-23 22:09:51 +0800

    added tomcat started in pom

--- src/main/java/com/project/main/CollectiveApplication.java
+++ src/main/java/com/project/main/CollectiveApplication.java
@@ -2,12 +2,18 @@ package com.project.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CollectiveApplication {
+public class CollectiveApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(CollectiveApplication.class, args);
 	}
 
+	@Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	      return builder.sources(CollectiveApplication.class);
+	  }
 }
